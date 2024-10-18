94a5a441d3f62b7e31ead8db8c45093f53dbc0b0
Dawid Sokol <sokol.dawid@SZKOLA.ZST-RADOM.EDU.PL>
2024-10-18 15:25:52 +0200

    to nie dawid

--- SD.pesel/PersonId.cs
+++ SD.pesel/PersonId.cs
@@ -13,29 +13,19 @@
             _id = id;
         }
 
-        /// <summary>
-        /// Get full year from PESEL
-        /// </summary>
         public int GetYear()
         {
             int year = int.Parse(_id.Substring(0, 2));
             int month = int.Parse(_id.Substring(2, 2));
 
-            if (month > 80) // 1800s
-                return year + 1800;
-            if (month > 60) // 2200s
-                return year + 2200;
-            if (month > 40) // 2100s
-                return year + 2100;
-            if (month > 20) // 2000s
-                return year + 2000;
-            // 1900s
+            if (month > 80) return year + 1800;
+            if (month > 60) return year + 2200;
+            if (month > 40) return year + 2100;
+            if (month > 20) return year + 2000;
+
             return year + 1900;
         }
 
-        /// <summary>
-        /// Get month from PESEL
-        /// </summary>
         public int GetMonth()
         {
             int month = int.Parse(_id.Substring(2, 2));
@@ -43,41 +33,28 @@
             if (month > 60) return month - 60;
             if (month > 40) return month - 40;
             if (month > 20) return month - 20;
+
             return month;
         }
 
-        /// <summary>
-        /// Get day from PESEL
-        /// </summary>
         public int GetDay()
         {
-            string day = _id.Substring(4, 2);
-            return int.Parse(day);
+            return int.Parse(_id.Substring(4, 2));
         }
 
-        /// <summary>
-        /// Get year of birth from PESEL
-        /// </summary>
         public int GetYearOfBirth()
         {
             return GetYear();
         }
 
-        /// <summary>
-        /// Get gender from PESEL
-        /// </summary>
         public string GetGender()
         {
             int genderDigit = int.Parse(_id[9].ToString());
-            return genderDigit % 2 == 0 ? "f" : "m"; // Even - female, odd - male
+            return genderDigit % 2 == 0 ? "k" : "m"; // k for female, m for male
         }
 
-        /// <summary>
-        /// Check if PESEL is valid
-        /// </summary>
         public bool IsValid()
         {
-            // Validate PESEL checksum
             int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
             int sum = 0;
 
